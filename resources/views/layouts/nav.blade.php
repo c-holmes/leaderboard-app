efcdebe37f7170e1f61ed5dd03eca65fe47d9519
c-holmes <chrisrobholmes@gmail.com>
2018-01-04 07:48:15 -0500

    init commit

---
+++ resources/views/layouts/nav.blade.php
@@ -0,0 +1,33 @@
+<nav class="navbar navbar-toggleable-md navbar-inverse bg-inverse fixed-top">
+  <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <a class="navbar-brand" href="#">Breakout</a>
+
+  <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Play <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Highscores</a>
+        <div class="dropdown-menu" aria-labelledby="dropdown01">
+          <a class="dropdown-item" href="#">Today</a>
+          <a class="dropdown-item" href="#">This Week</a>
+          <a class="dropdown-item" href="#">This Month</a>
+          <a class="dropdown-item" href="#">All Time</a>
+        </div>
+      </li>
+    </ul>
+    
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="#">Login</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Register</a>
+      </li>
+    </ul>
+  </div>
+</nav>
---
+++ routes/web.php
@@ -0,0 +1,2 @@
+<?php
+Route::get('/', 'GameController@index')->name('home');
